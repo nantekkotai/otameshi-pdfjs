2b2d1deac3f898bae82b25d6f46f6aa45afd3377
Nobuyuki OSAWA <nantekkotai.nobu@gmail.com>
2016-03-04 17:54:38 +0900

    やっとまともに動いた

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,71 @@
 import gulp from 'gulp';
+import jade from 'gulp-jade';
+import sass from 'gulp-sass';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import browserify from 'browserify'
+import babelify from 'babelify';
+import runSequence from 'run-sequence';
 import browserSync from 'browser-sync';
+// browserifyのstreamをgulp向けに変換するんだってさ
+import source from 'vinyl-source-stream';
+import _ from 'lodash';
 
-const showcasePath = './showcase/'
+const dir = {
+  dist: './dist',
+  src: './src'
+}
+
+const options = {
+  serve: {
+    open: false,
+    server: {
+      baseDir: dir.dist
+    }
+  },
+  
+  sass: {
+    outputstyle: 'expanded'
+  }
+}
+
+gulp.task('jade', () => {
+  return gulp.src([`${dir.src}/**/*.jade`, `!${dir.src}/**/_*.jade`])
+          .pipe(plumber({
+            errorHandler: notify.onError('<%= error.message %>')
+          }))
+          .pipe(jade())
+          .pipe(gulp.dest(dir.dist));
+});
+
+gulp.task('script', () => {
+  browserify(`${dir.src}/app.js`)
+    .transform(babelify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest(`${dir.dist}`));
+});
+
+gulp.task('css', () => {
+  return gulp.src([`${dir.src}/**/*.scss`, `!${dir.src}/**/_*.scss`])
+          .pipe(plumber({
+            errorHandler: notify.onError('<%= error.message %>')
+          }))
+          .pipe(sass(options.sass))
+          .pipe(gulp.dest(dir.dist));
+});
 
 gulp.task('serve', () => {
-  browserSync.init({
-    open: false,
-    server: showcasePath
-  });
+  browserSync.init(options.serve);
 
-  gulp.watch(`${showcasePath}**`, () =>{
+  gulp.watch(`${dir.src}/**`, ['build']);
+  gulp.watch(`${dir.dist}/**`, () => {
     browserSync.reload();
   });
 });
 
-gulp.task('default', ['serve']);
+gulp.task('build', callback => {
+  runSequence('jade', 'script', 'css', callback);
+});
+
+gulp.task('default', ['build', 'serve']);
---
+++ src/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(() => {
+  console.warn("警告だよ！");
+});
